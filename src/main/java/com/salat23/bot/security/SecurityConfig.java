44ff7cad56e64e00101eab10d6e9d2c119d39358
salat23 <misikon@mail.ru>
2022-04-18 10:35:47 +0300

    Added test handle and auth

--- src/main/java/com/salat23/bot/security/SecurityConfig.java
+++ src/main/java/com/salat23/bot/security/SecurityConfig.java
@@ -14,6 +14,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .anyRequest().authenticated();
+                .antMatchers("/**").hasAnyAuthority();
     }
 }
